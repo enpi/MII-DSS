024f960cc1b91688ccfffb400c5d2ceead154273
enpi <juammanc@hotmail.com>
2015-11-28 23:34:43 +0100

    Actualización práctica 3

---
+++ Práctica 3/practica3DSS/src/dss/practica3/modelo/Todo.java
@@ -0,0 +1,84 @@
+package dss.practica3.modelo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Todo{
+	
+	// Películas
+	
+	private String id;
+	private String titulo;
+	private String anio;
+	private String genero;
+	private String director;
+	private String actores;
+	private String sinopsis;
+	
+	public Todo(){
+	}
+	
+	public Todo (String id, String titulo){
+		this.id = id;
+		this.titulo = titulo;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getSinopsis() {
+		return sinopsis;
+	}
+
+
+	public void setSinopsis(String sinopsis) {
+		this.sinopsis = sinopsis;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getAnio() {
+		return anio;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public String getActores() {
+		return actores;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setAnio(String anio) {
+		this.anio = anio;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public void setActores(String actores) {
+		this.actores = actores;
+	}
+	
+	
+}
---
+++ Práctica 3/practica3DSS/src/dss/practica3/recurso/TodosResource.java
@@ -0,0 +1,145 @@
+package dss.practica3.recurso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+import dss.practica3.modelo.Todo;
+import dss.practica3.modelo.TodoDao;
+
+
+// Will map the resource to the URL todos
+@Path("/todos")
+public class TodosResource {
+
+  // Allows to insert contextual objects into the class,
+  // e.g. ServletContext, Request, Response, UriInfo
+  @Context
+  UriInfo uriInfo;
+  @Context
+  Request request;
+	
+	  // Return the list of todos to the user in the browser
+	  @GET
+	  @Produces(MediaType.TEXT_XML)
+	  public List<Todo> getTodosBrowser() {
+	    List<Todo> todos = new ArrayList<Todo>();
+	    todos.addAll(TodoDao.INSTANCE.getModelo().values());
+	    return todos;
+	  }
+
+	  // Return the list of todos for applications
+	  @GET
+	  @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	  public List<Todo> getTodos() {
+	    List<Todo> todos = new ArrayList<Todo>();
+	    todos.addAll(TodoDao.INSTANCE.getModelo().values());
+	    return todos;
+	  }
+
+	  // retuns the number of todos
+	  // Use http://localhost:8080/com.vogella.jersey.todo/rest/todos/count
+	  // to get the total number of records
+	  @GET
+	  @Path("count")
+	  @Produces(MediaType.TEXT_PLAIN)
+	  public String getCount() {
+	    int count = TodoDao.INSTANCE.getModelo().size();
+	    return String.valueOf(count);
+	  }
+	
+	
+	@POST
+	  @Produces(MediaType.TEXT_HTML)
+	  @Consumes(MediaType.MULTIPART_FORM_DATA)
+	  public void newTodo(@FormParam("id") String id,
+	      @FormParam("titulo") String titulo,
+	      @FormParam("anio") String anio,
+	      @FormParam("genero") String genero,
+	      @FormParam("director") String director,
+	      @FormParam("actores") String actores,
+	      @FormParam("sinopsis") String sinopsis,
+
+	      //@FormDataParam("imagen") InputStream uploadedInputStream,
+		  //@FormDataParam("imagen") FormDataContentDisposition fileDetail,
+		  
+	      @Context HttpServletResponse servletResponse) throws IOException{
+	    Todo todo = new Todo(id, titulo);
+	    if (anio != null && genero != null && director != null && actores != null && sinopsis != null ) {
+	    	todo.setAnio(anio);
+	    	todo.setGenero(genero);
+	    	todo.setDirector(director);
+	    	todo.setActores(actores);
+	    	todo.setSinopsis(sinopsis);
+	    	
+	    }
+	    /*
+	    if(fileDetail !=null){
+	    	String uploadedFileLocation = "/home/iv-aerospace/Escritorio/" + fileDetail.getFileName();
+
+			// save it
+			writeToFile(uploadedInputStream, uploadedFileLocation);
+
+			String output = "File uploaded to : " + uploadedFileLocation;
+			todo.setImagen(uploadedFileLocation);
+	    }
+	    */
+	    
+	    TodoDao.INSTANCE.getModelo().put(id, todo);
+
+	    servletResponse.sendRedirect("../todo/res");
+	  }
+
+	  // Defines that the next path parameter after todos is
+	  // treated as a parameter and passed to the TodoResources
+	  // Allows to type http://localhost:8080/com.vogella.jersey.todo/rest/todos/1
+	  // 1 will be treaded as parameter todo and passed to TodoResource
+	  @Path("{todo}")
+	  public TodoResource getTodo(@PathParam("todo") String id) {
+	    return new TodoResource(uriInfo, request, id);
+	  }
+	  
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream,
+		String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+} 
